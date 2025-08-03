1cc54edcf7065cbc38c4f40efd2eac9c335de5d4
Kalpesh Bhatkar <kalpeshbhatkar@outlook.com>
2025-08-03 11:30:55 +0530

    Part 3 Component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { EmployeeListComponent } from './employee-list/employee-list.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [AddEmployeeComponent,EmployeeListComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-list',
+  imports: [],
+  template: '<h1 class="text-primary">Employee List</h1>',
+  styles: ['.primary{color:red}']
+})
+export class EmployeeListComponent {
+
+}
