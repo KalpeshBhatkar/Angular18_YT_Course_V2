416f28ad1cb718a73c5454c5edcb1fca3a177fd1
Kalpesh Bhatkar <kalpeshbhatkar@outlook.com>
2025-08-11 21:58:12 +0530

    Part 10 Default Pipe & Custom Pipe

---
+++ src/app/components/pipe/pipe.component.ts
@@ -0,0 +1,31 @@
+import { AsyncPipe, DatePipe, JsonPipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { interval,  Observable, map } from 'rxjs';
+import { NaPipe } from '../../pipes/na.pipe';
+
+@Component({
+  selector: 'app-pipe',
+  imports: [UpperCasePipe,LowerCasePipe,TitleCasePipe,DatePipe,JsonPipe,AsyncPipe,NaPipe],
+  templateUrl: './pipe.component.html',
+  styleUrl: './pipe.component.css'
+})
+export class PipeComponent {
+
+  firstName1: string = "Angular";
+  firstName2: string = "this is a demo session";
+
+  currentDate: Date = new Date();
+
+  currentTime: Observable<Date> = new Observable<Date>;
+
+  student: any = {
+    name:'Kalpesh',
+    city:'Mumbai',
+    empId:34,
+    state:''
+  }
+
+  constructor(){
+      this.currentTime = interval(1000).pipe(map(() => new Date()));
+  }
+}
