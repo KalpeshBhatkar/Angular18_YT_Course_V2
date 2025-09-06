3b034fdf1fd43eb056e6199d45f90ab119eba23e
Kalpesh Bhatkar <kalpeshbhatkar@outlook.com>
2025-09-06 21:42:52 +0530

    Part 26 Constant File in Angular

---
+++ src/app/constant/Constant.ts
@@ -0,0 +1,15 @@
+export const Constant = {
+    API_URL: "https://jsonplaceholder.typicode.com/",
+
+    DEPARTMENT_METHODS: {
+        GET_PARENT_DEPT: "posts",
+        ADD_NEW_DEPT: "users"
+    },
+
+    VALIDATION_MESSAGE: {
+        REQUIRED: "This is required",
+        MIN_LENGTH: "Min 3 Char Require"
+    },
+
+    STATUS_LIST: ['TO DO', 'In-progress']
+}
--- src/app/service/department.service.ts
+++ src/app/service/department.service.ts
@@ -1,20 +1,21 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Constant } from '../constant/Constant';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepartmentService {
 
-  apiURL: string = "https://jsonplaceholder.typicode.com/";
+  //apiURL: string = "https://jsonplaceholder.typicode.com/";
   constructor(private http: HttpClient) { }
 
   getAllDept(){
-    return this.http.get(this.apiURL + "posts");
+    return this.http.get(Constant.API_URL + Constant.DEPARTMENT_METHODS.ADD_NEW_DEPT);
   }
 
   saveNewDept(obj: any){
-    return this.http.post(`${this.apiURL}posts`,obj);
+    return this.http.post(`${Constant.API_URL}${Constant.DEPARTMENT_METHODS.ADD_NEW_DEPT}`,obj);
   }
 
   addTwoNo(num1:number, num2:number){
