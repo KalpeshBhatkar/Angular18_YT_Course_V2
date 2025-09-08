6d5cd9cecf4189959a3ec1a87d21a6dae85a507a
Kalpesh Bhatkar <kalpeshbhatkar@outlook.com>
2025-09-08 22:09:38 +0530

    Part 30 What is Change Detection and zone.js

--- src/app/add-employee/add-employee.component.ts
+++ src/app/add-employee/add-employee.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, inject, OnInit, signal } from '@angular/core';
 
 @Component({
   selector: 'app-add-employee',
   imports: [],
   templateUrl: './add-employee.component.html',
-  styleUrl: './add-employee.component.css'
+  styleUrl: './add-employee.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AddEmployeeComponent {
+export class AddEmployeeComponent implements OnInit {
 
+  http = inject(HttpClient);
+
+  //userList: any[] = [];
+  name: string = 'HTML';
+
+  userList = signal<any[]>([]);
+
+  constructor(private cd:ChangeDetectorRef){
+
+  }
+
+
+  ngOnInit(): void {
+    this.http.get("https://json-placeholder.mock.beeceptor.com/users").subscribe((result:any)=>{
+      //this.userList = result;
+      this.userList.set(result);
+      this.name = 'JAVA';
+      // setTimeout(() => {
+      //   this.cd.detectChanges();
+      // }, 5000);
+    });
+  }
+
+  reloadUI() {
+
+  }
 }
